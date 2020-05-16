9c6444f7cbfac0c168470ba7067bfd90e4543b97
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-05-16 17:10:19 +0700

    Add project files.

---
+++ Total Berjalan/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.labelTotal = New System.Windows.Forms.Label()
+        Me.tombolMasukkan = New System.Windows.Forms.Button()
+        Me.tombolKeluar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Penjualan Total:"
+        '
+        'labelTotal
+        '
+        Me.labelTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelTotal.Location = New System.Drawing.Point(103, 13)
+        Me.labelTotal.Name = "labelTotal"
+        Me.labelTotal.Size = New System.Drawing.Size(169, 23)
+        Me.labelTotal.TabIndex = 1
+        '
+        'tombolMasukkan
+        '
+        Me.tombolMasukkan.Location = New System.Drawing.Point(16, 78)
+        Me.tombolMasukkan.Name = "tombolMasukkan"
+        Me.tombolMasukkan.Size = New System.Drawing.Size(125, 38)
+        Me.tombolMasukkan.TabIndex = 2
+        Me.tombolMasukkan.Text = "Masukkan Penjualan"
+        Me.tombolMasukkan.UseVisualStyleBackColor = True
+        '
+        'tombolKeluar
+        '
+        Me.tombolKeluar.Location = New System.Drawing.Point(165, 78)
+        Me.tombolKeluar.Name = "tombolKeluar"
+        Me.tombolKeluar.Size = New System.Drawing.Size(107, 38)
+        Me.tombolKeluar.TabIndex = 3
+        Me.tombolKeluar.Text = "Keluar"
+        Me.tombolKeluar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 132)
+        Me.Controls.Add(Me.tombolKeluar)
+        Me.Controls.Add(Me.tombolMasukkan)
+        Me.Controls.Add(Me.labelTotal)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Total Berjalan"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents labelTotal As System.Windows.Forms.Label
+    Friend WithEvents tombolMasukkan As System.Windows.Forms.Button
+    Friend WithEvents tombolKeluar As System.Windows.Forms.Button
+
+End Class
---
+++ Total Berjalan/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+
+    Private Sub tombolMasukkan_Click(sender As Object, e As EventArgs) Handles tombolMasukkan.Click
+        Const intBANYAK_HARI As Integer = 5 'banyak hari
+        Dim intHitung As Integer = 1        'pencacah loop
+        Dim decPenjualan As Decimal = 0     'memuat penjualan harian
+        Dim decTotal As Decimal = 0         'memuat penjualan total
+        Dim strMasukan As String            'memuat masukan string
+
+        'membaca penjualan tiap hari
+        Do While intHitung <= intBANYAK_HARI
+            'membaca penjualan harian dari user
+            strMasukan = InputBox("Masukkan nilai penjualan untuk hari " &
+            intHitung.ToString())
+
+            'mengkonversi masukan menjadi sebuah Decimal
+            If Decimal.TryParse(strMasukan, decPenjualan) Then
+                'menambahkan penjualan harian pada penjualan total
+                decTotal += decPenjualan
+
+                'menambahkan 1 pada pencacah loop
+                intHitung += 1
+            Else
+                'menampilkan pesan error karena masukan tak valid
+                MessageBox.Show("Masukkan nilai numerik.")
+            End If
+        Loop
+
+        'menampilkan penjualan total
+        labelTotal.Text = "Rp. " & decTotal.ToString()
+    End Sub
+
+    Private Sub tombolKeluar_Click(sender As Object, e As EventArgs) Handles tombolKeluar.Click
+        'menutup form
+        Me.Close()
+    End Sub
+End Class
